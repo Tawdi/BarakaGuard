963b104c79a33b67371eaa23c94d6cc7da1d6521
Tawdi <ahmedtawdi00@gmail.com>
2025-09-29 16:37:23 +0100

    BAR-21 Interface ClientDAO

--- src/main/java/com/barakaguard/dao/ClientDAO.java
+++ src/main/java/com/barakaguard/dao/ClientDAO.java
@@ -1,4 +1,12 @@
 package main.java.com.barakaguard.dao;
 
-public interface ClientDAO {
+import java.util.Optional;
+import java.util.UUID;
+
+import main.java.com.barakaguard.entity.client.Client;
+
+public interface ClientDAO extends BaseDAO<Client, UUID> {
+
+    Optional<Client> findByEmail(String email);
+
 }
