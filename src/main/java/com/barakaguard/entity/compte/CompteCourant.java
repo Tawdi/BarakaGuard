9a3996ab00daa884f27e32702f0f2cfea47a0cfd
Tawdi <ahmedtawdi00@gmail.com>
2025-09-29 16:21:30 +0100

    BAR-7 Créer CompteCourant (découvert autorisé)

--- src/main/java/com/barakaguard/entity/compte/CompteCourant.java
+++ src/main/java/com/barakaguard/entity/compte/CompteCourant.java
@@ -1,4 +1,32 @@
 package main.java.com.barakaguard.entity.compte;
 
-public class CompteCourant {
+import java.util.UUID;
+
+public final class CompteCourant extends Compte {
+
+    private double decouvert;
+
+    public CompteCourant(UUID id, String numero, double solde, UUID idClient, double decouvertAutorise) {
+        super(id, numero, solde, idClient);
+        setDecouvert(decouvertAutorise);
+    }
+
+    public void setDecouvert(double decouvertAutorise) {
+        this.decouvert = decouvertAutorise;
+    }
+
+    public double getDecouvertAutorise() {
+        return decouvert;
+    }
+
+    @Override
+    public String toString() {
+        return "CompteCourant{" +
+                "id=" + getId() +
+                ", numero='" + getNumero() + '\'' +
+                ", solde=" + getSolde() +
+                ", idClient=" + getIdClient() +
+                ", decouvertAutorise=" + decouvert +
+                '}';
+    }
 }
