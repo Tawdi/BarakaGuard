7b5d4f114fe2a7b05e2ab6ef0480caae500cf596
Tawdi <ahmedtawdi00@gmail.com>
2025-09-29 16:28:37 +0100

    BAR-9 Créer Transaction record (id, date, montant, type, lieu, idCompte)

--- src/main/java/com/barakaguard/entity/transaction/Transaction.java
+++ src/main/java/com/barakaguard/entity/transaction/Transaction.java
@@ -1,4 +1,13 @@
 package main.java.com.barakaguard.entity.transaction;
 
-public class Transaction {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Transaction(
+        UUID id,
+        LocalDateTime date,
+        double montant,
+        TypeTransaction type,
+        String lieu,
+        UUID idCompte) {
 }
