0eca9e68bcff7d8de576b836e197840b82cb8370
Tawdi <ahmedtawdi00@gmail.com>
2025-09-29 16:37:44 +0100

    BAR-23 Interface CompteDAO

--- src/main/java/com/barakaguard/dao/CompteDAO.java
+++ src/main/java/com/barakaguard/dao/CompteDAO.java
@@ -1,4 +1,14 @@
 package main.java.com.barakaguard.dao;
 
-public interface CompteDAO {
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import main.java.com.barakaguard.entity.compte.Compte;
+
+public interface CompteDAO extends BaseDAO<Compte, UUID> {
+
+    List<Compte> findByClientId(UUID clientId);
+
+    Optional<Compte> findByNumero(String numero);
 }
