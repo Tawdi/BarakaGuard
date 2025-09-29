2a267c973bde9d84fe94e527d6cea10e64cde6fb
Tawdi <ahmedtawdi00@gmail.com>
2025-09-29 16:22:33 +0100

    BAR-8 Créer CompteEpargne (taux d’intérêt)

--- src/main/java/com/barakaguard/entity/compte/CompteEpargne.java
+++ src/main/java/com/barakaguard/entity/compte/CompteEpargne.java
@@ -1,4 +1,33 @@
 package main.java.com.barakaguard.entity.compte;
 
-public class CompteEpargne {
+import java.util.UUID;
+
+public final class CompteEpargne extends Compte {
+
+    private double tauxInteret;
+
+    public CompteEpargne(UUID id, String numero, double solde, UUID idClient, double tauxInteret) {
+        super(id, numero, solde, idClient);
+        setTauxInteret(tauxInteret);
+    }
+
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+
+    @Override
+    public String toString() {
+        return "CompteEpargne{" +
+                "id=" + getId() +
+                ", numero='" + getNumero() + '\'' +
+                ", solde=" + getSolde() +
+                ", idClient=" + getIdClient() +
+                ", tauxInteret=" + tauxInteret +
+                '}';
+    }
 }
--- src/main/java/com/barakaguard/entity/compte/compte.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.barakaguard.entity.compte;
-
-public class compte {
-}
