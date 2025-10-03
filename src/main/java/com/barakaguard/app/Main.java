e2282e0faa209c30dcae83a341672181cd20fb52
Tawdi <ahmedtawdi00@gmail.com>
2025-10-03 09:16:57 +0100

    BAR-51 Menu Principal

--- src/main/java/com/barakaguard/app/Main.java
+++ src/main/java/com/barakaguard/app/Main.java
@@ -2,9 +2,12 @@ package main.java.com.barakaguard.app;
 
 import java.time.LocalDate;
 
+import main.java.com.barakaguard.ui.MenuPrincipal;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.afficher();
     }
 }
--- src/main/java/com/barakaguard/ui/MenuPrincipal.java
+++ src/main/java/com/barakaguard/ui/MenuPrincipal.java
@@ -1,4 +1,38 @@
 package main.java.com.barakaguard.ui;
 
+import java.util.Scanner;
+
+import main.java.com.barakaguard.util.InputUtil;
+
 public class MenuPrincipal {
+
+    private final MenuClient menuClient = new MenuClient();
+    private final MenuCompte menuCompte = new MenuCompte();
+    private final MenuTransaction menuTransaction = new MenuTransaction();
+
+    public void afficher() {
+        boolean quitter = false;
+
+        while (!quitter) {
+            System.out.println("\n=== MENU PRINCIPAL ===");
+            System.out.println("1. Gérer les clients");
+            System.out.println("2. Gérer les comptes");
+            System.out.println("3. Gérer les transactions");
+            System.out.println("4. Quitter");
+            System.out.print("Choix: ");
+
+           int choix = InputUtil.readInt("Choix", 1, 4);
+
+            switch (choix) {
+                case 1 -> menuClient.afficher();
+                case 2 -> menuCompte.afficher();
+                case 3 -> menuTransaction.afficher();
+                case 4 -> {
+                    System.out.println("Au revoir !");
+                    quitter = true;
+                }
+                default -> System.out.println("Choix invalide !");
+            }
+        }
+    }
 }
